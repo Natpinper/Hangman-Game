ccc31973a57731128e8190d35ced7558b0540286
Natalia Pinto <pinto.natalia@outlook.com>
2023-12-26 19:26:47 +0100

    starGame Function completed

---
+++ src/hangman.js
@@ -0,0 +1,50 @@
+class HangmanGame {
+    constructor(){
+        this.startScreen = document.getElementById("game-intro");
+        this.gameScreen = document.getElementById("game-container");
+        this.gameEndScreen = document.getElementById("game-over");
+        this.words=allWords
+        this.lettersGuessed=[];
+        this.lettersFailed=0;
+        this.pickedWord=[];
+        this.chosenWord=this.pickedWord.word
+        
+    }
+   
+   pickWord(){
+    const randomIndex= Math.floor(Math.random()*this.words.length)
+    this.pickedWord = this.words[randomIndex]
+    return this.chosenWord= this.pickedWord.word
+    }
+    
+    createList(){
+        for(let i=0; i<this.chosenWord.length; i++){
+        const ul= document.getElementById("lines")
+        const li= document.createElement('li');
+        ul.appendChild(li)
+        } 
+    }
+    showHint(){
+        const hintText= document.getElementById('hint')
+        hintText.innerHTML += `${this.pickedWord.hint}` 
+    }
+   chekIfCommonLetters(element,letter){
+        for(let i=0; i<this.chosenWord.length; i++){
+            if(this.chosenWord.includes(letter)){
+             return this.lettersGuessed.splice(this.chosenWord[i],0,letter), true
+            }else if(!this.chosenWord.includes(letter)){
+            return this.lettersFailed++ , false
+            }
+        }
+    }
+
+    checkIfFinished(){
+        if (this.lettersFailed===6){
+            return true
+        }if(this.lettersGuessed===this.pickedWord.split()){
+            return true
+        }if(this.lettersFailed<6 && this.lettersGuessed!==this.pickedWord.split()){
+            return false
+        }
+    }
+}
